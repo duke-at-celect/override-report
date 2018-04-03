ac21499a164691e4233c8aed2126cc813cde3369
duke-at-celect <33836287+duke-at-celect@users.noreply.github.com>
2018-04-03 09:49:06 -0400

    Update override_report_cleanup.py
    Merged the cleanup and the merged code into one script

--- override_report_cleanup.py
+++ override_report_cleanup.py
@@ -1,75 +1,75 @@
 import pandas as pd
-import numpy as np
-import argparse
+import glob
 
 
-def read_data():
-	rawData = file
-	df = pd.read_csv(rawData)
+def path():
+    files_path = input("\nFull path to the override files (.csv) that need to be merged: ")
 
-	# Create and fill new type columns to track store type
-	df['celect_fulfill_store_type'] = 'store'
-	df['oms_fulfill_store_type'] = 'store'
-
-	# Compare celectFulfilledStore to OMSFulfilledStore - if values don't match Celect has been overridden.
-	or_df = df[df.celectFulfilledStore != df.OMSFulfilledStore].sort_values(['orderCreateDate', 'celectFulfilledStore'])
-	or_df.dropna(subset=['OMSFulfilledStore'], inplace=True)
-
-	return or_df
+    return files_path
 
 
-def truncate (or_df):
-	# Assign store_type
-	# print(x.head(2))  # debug
-	if region == 'us':
-		# set DCs and MHs to united states values
-		dc = [9920, 9923]
-		mh = [2283]
-	else:
-		# set DCs and MHs to Canadian values
-		dc = [9910, 9911]
-		mh = [1488]
-
-	or_df.loc[or_df['celectFulfilledStore'].isin(dc), 'celect_fulfill_store_type'] = 'distribution center ({})'.format(region)
-	or_df.loc[or_df['celectFulfilledStore'].isin(mh), 'celect_fulfill_store_type'] = 'mini-hub ({})'.format(region)
-
-	new_or_df = or_df[['brandName', 'orderId', 'orderCreateDate', 'orderCreateDateTime', 'sku', 'inventoryVal',
-	                   'celectFulfilledStore', 'celect_fulfill_store_type', 'OMSFulfilledStore',
-	                   'oms_fulfill_store_type']]
-
-	# new_or_df.drop_duplicates(subset=['orderId', 'sku'], keep='last')
-	# print(new_or_df.head(5))
-
-	new_or_df.to_csv('~/Downloads/aldo/prod/override_reports/{}_truncated_override.csv'.format(region),  sep=',')
+def merge_csv(files_path):
+    all_files = glob.glob(files_path + '*.csv')
+    list_ = []
+    for file_ in all_files:
+        df = pd.read_csv(file_, index_col=None, header=0)
+        list_.append(df)
+    merged_data = pd.concat(list_)
+    merged_data.drop_duplicates(inplace=True)
+    brand = merged_data.iloc[0]['brandName']
+    output = files_path + '{}_cat_result.csv'.format(brand)
+    merged_data = merged_data[['brandName', 'orderId', 'orderCreateDate',
+                           'orderCreateDateTime', 'orderPostDate', 'orderPostTimeStamp', 'orderShipmentId',
+                           'shipmentShippingCost', 'sku', 'zipcode', 'celectFulfilledStore', 'shipToStoreFlag',
+                           'shipToStoreId', 'orderedQuantity', 'inventoryVal', 'quantitySentStore',
+                           'pickDecline', 'weeksSupply', 'isOnesie', 'storeResponseShipmentId',
+                           'OMSFulfilledStore', 'respondDateTime', 'responseTimeMins', 'quantityDeclined',
+                           'quantityShipped']]
+    # Check if the file already exists? Prompt the user if it does?
+    merged_data.to_csv(output)
+    return merged_data
 
 
-parser = argparse.ArgumentParser(description = "Use only the 'complete_store_reponse_w_details..' override report file with this script." )
-parser.add_argument('--region',  choices=['ca', 'us'], help="Must be either 'ca' for Canada or 'us' for United States")
-parser.add_argument("--file",  help="Used to pass the file to process - include path if necessary")
-args = parser.parse_args()
-region = args.region
-file = args.file
+def read_data(merged_data):
+    # Pass 'merged_data' to this function from merge_csv()
+    df = merged_data
+    # Create and fill new type columns to track store type
+    df['celect_fulfill_store_type'] = 'store'
+    df['oms_fulfill_store_type'] = 'store'
+    # Compare celectFulfilledStore to OMSFulfilledStore - if values don't match Celect has been overridden.
+    all_data = df[df.celectFulfilledStore != df.OMSFulfilledStore].sort_values(['orderCreateDate', 'celectFulfilledStore'])
+    all_data.dropna(subset=['OMSFulfilledStore'], inplace=True)
+    return all_data
 
-if file.endswith('.csv'):
-	print("\nProcessing file...")
-else:
-	print("\nIncorrect file format. Exiting.")
-	exit()
 
-if region == 'us':
-	or_df=read_data()
-	brand = or_df.iloc[0] [ 'brandName']
-	if brand != 'aldo_us':
-		print('ERROR: Data in file is for brand:', brand, 'and --region was set to:', region)
-		exit()
-	else:
-		truncate(or_df)
+def truncate(all_data):
+    # Pass read_data() output to this function
+    # path = or_files
+    brand = all_data.iloc[0]['brandName']
+    if brand == 'aldo_us':
+        # set DCs and MHs to United States values
+        dc = [9920, 9923]
+        mh = [2283]
+    else:
+        # set DCs and MHs to Canadian values
+        dc = [9910, 9911]
+        mh = [1488]
 
-if region == 'ca':
-	or_df=read_data()
-	brand = or_df.iloc[0] [ 'brandName']
-	if brand != 'aldo_ca':
-		print('ERROR: Data in file is for region: Unites States but the --region flag was set to: {}\n'.format(region))
-		exit()
-	else:
-		truncate(or_df)
+    all_data.loc[all_data['celectFulfilledStore'].isin(dc),
+              'celect_fulfill_store_type'] = 'distribution center ({})'.format(brand)
+    all_data.loc[all_data['celectFulfilledStore'].isin(mh),
+              'celect_fulfill_store_type'] = 'mini-hub ({})'.format(brand)
+    new_all_data = all_data[['brandName', 'orderId', 'orderCreateDate', 'orderCreateDateTime', 'sku', 'inventoryVal',
+                       'celectFulfilledStore', 'celect_fulfill_store_type', 'OMSFulfilledStore',
+                       'oms_fulfill_store_type']]
+    new_all_data.to_csv(files_path + '{}_truncated_override.csv'.format(brand), sep=',')
+
+
+files_path = path()
+print('Starting Merge...')
+merged_data = merge_csv(files_path)
+print('Reading Merged Data...')
+all_data = read_data(merged_data)
+print('Truncating Data...')
+truncate(all_data)
+print('Processing Complete.\n')
